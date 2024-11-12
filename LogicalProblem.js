9693a440784cd447142eb74a118ba7c4b7ded3f7
Saikat <saikat.blast20@gmail.com>
2024-11-13 03:45:53 +0600

    Logical problem

--- LogicalProblem.js
+++ LogicalProblem.js
@@ -7,11 +7,45 @@
 // 2. Find a function to check if a given number is prime 
 // Prime Number:A prime number is a positive integer that can only be divided evenly by itself and the number 1
 
-function isPrime (num){
-    if(num<1) return false;
-    for(let i = 2; i<=Math.sqrt(num);i++){
-        if(num %i ===0 ) return false;
-    }
-    return true;
+// function isPrime (num){
+//     if(num<1) return false;
+//     for(let i = 2; i<=Math.sqrt(num);i++){
+//         if(num %i ===0 ) return false;
+//     }
+//     return true;
+// }
+// console.log(isPrime(7));
+
+// *** Reverse a String 
+// function  reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+// console.log(reverseString("javascript"));
+
+// ****write a function that retuns the largerst number in an array 
+// function largerstNumber(arr){
+//     return Math.max(...arr);
+// }
+// console.log(largerstNumber([1,2,35,6,80]));
+
+
+
+//*** */ Write a function to generate the fist n numbers of the Fibonacci sequence 
+//  fibonacci ager dui tar addition porer ta 
+
+// function fibonacci(n){
+//     let fib = [0,1];
+//     for(let i = 2; i<n; i++){
+//         fib[i] = fib[i-1]+fib[i -2];
+//     }
+//     return fib;
+// }
+// console.log(fibonacci(5));
+
+//***  */ write a function that retuns the Factorial of a given number 
+
+function factorial(num){
+    if(num ===0 || num ===1)return 1;
+    return num * factorial( num -1);
 }
-console.log(isPrime(7));
+console.log(factorial(5));
--- promise2.js
+++ promise2.js
@@ -149,24 +149,24 @@
 
 
 
-// function asyncFunc1(){
-//     return new Promise((resolve, reject) =>{
-//         setTimeout(()=>{
-//             console.log(" data 1");
-//             resolve("success");
-//         },4000);
-//     });
-// }
+function asyncFunc1(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log(" data 1");
+            resolve("success");
+        },4000);
+    });
+}
 
 
-// function asyncFunc2(){
-//     return new Promise((resolve, reject) =>{
-//         setTimeout(()=>{
-//             console.log(" data 2");
-//             resolve("success");
-//         },4000);
-//     });
-// }
+function asyncFunc2(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log(" data 2");
+            resolve("success");
+        },4000);
+    });
+}
 
 // console.log("fetching data1....");
 // let p1 = asyncFunc1();
@@ -182,7 +182,7 @@
 // console.log("fetching data1.....");
 // asyncFunc1().then((res)=>{
 //     console.log("fetching data2...");
-//     asyncFunc2.then((res)=>{
+//     asyncFunc2().then((res)=>{
 
 //     });
 // });
