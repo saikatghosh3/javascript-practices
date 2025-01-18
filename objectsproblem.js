9e430f6a92aabf431869f3e0ed8e6a8fa8223d48
Saikat Ghosh <saikat.blast20@gmail.com>
2025-01-19 01:14:25 +0600

    object

--- objectsproblem.js
+++ objectsproblem.js
@@ -40,8 +40,46 @@
 // }
 
 
-let obj1 = {a: 1, b: 2};
-let obj2 = { c: 3, b: 4};
+// let obj1 = {a: 1, b: 2};
+// let obj2 = { c: 3, b: 4};
 
-let merged = { ...obj1, ...obj2};
-console.log(merged);
+// let merged = { ...obj1, ...obj2};
+// console.log(merged);
+
+// problem
+// Count the number of properties in an object.
+
+//  const countProperties = obj => Object.keys(obj).length;
+//  const obj = {
+//     a:1, b:2, c:3
+//  };
+//  console.log(countProperties(obj));
+ 
+
+// problem
+// Convert an object into an array of its key-value pairs.
+
+// const obj = {a:1, b:2, c:4};
+// const keyValueArray = Object.entries(obj);
+// console.log(keyValueArray);
+
+// problems
+// write a function to check if an object is empty 
+
+// const isEmpty = obj => Object.keys(obj).length ===0;
+// console.log(isEmpty({}));
+// console.log(isEmpty({a:1}));
+
+// problem 
+// convert an array of key-value paris into an object 
+// const keyValueArray = [['a', 1], ['b', 2], ['c', 3]];
+// const obj = Object.fromEntries(keyValueArray);
+// console.log(obj);
+
+// problems
+// Reverse the keys adn values of an object 
+//  const reverseObject = obj => 
+//     Object.fromEntries(Object.entries(obj).map(([ key, value])=>[value, key]));
+//  const obj = {a:1, b:2, c:3};
+//  console.log(reverseObject(obj));
+ 
