84fd9cfd5e73ef0438efa2fb019a775d6254c665
Saikat Ghosh <saikat.blast20@gmail.com>
2025-02-27 15:34:14 +0600

    practice

--- newpractice.js
+++ newpractice.js
@@ -29,16 +29,75 @@
 // console.log(notMatch);
 
 // filter . when we wnat all the element that matches the condition . returns a new array.
-const numbers4 = [1,3,4,5,6,7]
-const matchCondition = numbers4.filter(num => num >3);
-console.log(matchCondition);
+// const numbers4 = [1,3,4,5,6,7]
+// const matchCondition = numbers4.filter(num => num >3);
+// console.log(matchCondition);
 // find index  retuns the first element that matches the condition 
 // if do not match it will return the -1
-const matchCondition1 = numbers4.findIndex(num => num > 3);
-console.log(matchCondition1)
+// const matchCondition1 = numbers4.findIndex(num => num > 3);
+// console.log(matchCondition1)
 
 // some : it cheks if any  element match the conditon
 // returns in true or false 
 
-const someElement = numbers4.some(num => num <2);
-console.log(someElement);
+// const someElement = numbers4.some(num => num <2);
+// console.log(someElement);
+
+
+// object : 
+// inside object: here  this refers to the obj
+const obj = {
+    name: "Saikat",
+    show: function () {
+      console.log(this.name);
+    },
+  };
+  
+  obj.show();
+ 
+//   inside arrow function : here this refers to outer scope means window scope.
+  const obj1 = {
+    name: "Saikat",
+    show: () => {
+      console.log(this.name);
+    },
+  };
+  
+  obj1.show();
+
+//   another example of arrow function 
+const user = {
+    name: "Saikat",
+    age: 25,
+
+showNormal: function () {
+    console.log(`Normal Function: ${this.name}, ${this.age}`);
+  },
+// as it is a normal function it this will refer to user 
+
+showWithArrowTimeout: function () {
+    setTimeout(() => {
+      console.log(`Timeout Arrow: ${this.name}, ${this.age}`);
+    }, 1000);  
+//  as it is arrow funciton it does't have this. it will take this from outer function.
+
+},
+}
+
+
+// Object.is 
+// Object.is() is a method used to compare two values to see if they are the same value . 
+// syntax:
+// Object.is(value1, value2);
+// true is the values are same 
+// false if the values are different
+
+// difference between === and Object.is 
+
+// === does not consider NaN equal to NaN 
+// Object.is() consider NaN equal to NaN
+
+// and
+
+// console.log(0 === -0);           // true
+// console.log(Object.is(0, -0));   // false
