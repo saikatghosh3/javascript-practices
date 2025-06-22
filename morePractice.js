beab68f0f408208439fc2864b5e6dde6aa118209
Saikat Ghosh <saikat.blast20@gmail.com>
2025-06-23 02:28:32 +0600

    basic

--- morePractice.js
+++ morePractice.js
@@ -16,3 +16,82 @@ for(let i = 1; i<=20; i++){
 const arr = [1,3,4,5,6,7,7,7,7,8,9,9,9]
 const unique = [...new Set(arr)];
 console.log(unique);
+
+// count the frequency of an Element 
+const arr = ["a", "b", "c", "b", "c,"];
+const freq = {};
+arr.forEach(item=> {
+    freq[item] = (freq[item] || 0) +1;
+});
+console.log(freq);
+
+// check Anagram (same letter, different order)
+
+function isAnagram(a,b){
+    return a.split('').sort().join('') === b.split('').sort().join('')
+}
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("hello", "world"))
+
+// reverse words in Sentence
+const sentece = "I love javascript";
+const reversed = sentece.split(' ').reverse().join(' ');
+console.log(reversed);
+
+// find intersection of Two arrays
+function intersection (arr1, arr2){
+    return arr1.filter(item => arr2.includes(item));
+}
+
+console.log(intersection([1, 2, 3], [2, 3, 4]));
+
+
+// more problem . 
+// Reverse the string without reverse
+ 
+function reverse(str){
+    let reversed = ""
+    for(char of str){
+        reversed = char + reversed;
+    // reversed er vitor last character first a jabe then tar ager ta ex olleh ei vabe
+    }
+    return reversed;
+}
+console.log(reverse("hello"));
+
+// find the Longest word in a sentence
+function longestWord (sentece){
+    const words = sentece.split(" ");
+    let Longest = "";
+    for( let word of words){
+       if(word.length > Longest.length){
+        Longest = word;
+       }
+    
+    }
+    return Longest;
+}
+console.log(longestWord("i love web development"));
+
+// count the vowel with regular exp
+function countVowel (str){
+    return str.match(/[aeiou]/gi)?length || 0 
+}
+console.log(countVowel("javaScript"));
+
+// check if a number is prime
+function isPrime(n){
+    if(n<=1 ) return false;
+    for(let i = 2; i<Math.sqrt(n); i++){
+        if(n % i=== 0 ) return false;
+    }
+    return true;
+}
+console.log(isPrime(7));
+
+// Capitalize first letter of sentence 
+function Capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    // str.slice(1) will return rest of the str
+}
+console.log(Capitalize("hello world"));
