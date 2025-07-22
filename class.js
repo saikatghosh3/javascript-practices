16c475810439a34d255ccfe135f5c76e7a0c88ef
Saikat Ghosh <saikat.blast20@gmail.com>
2025-07-23 02:57:36 +0600

    class-and-object-problem-solving

--- class.js
+++ class.js
@@ -121,3 +121,78 @@ const n  = new Employee("tonoy", 5000);
 console.log(`${n.name}'s salary is ${n.salary}Tk`)
 const m = new Manager("saikat", 5000, 1000);
 console.log(`${m.name}'s total salary: ${m.totalSalary() }Tk`);
+
+// create a book class with properties title, author isAvailable
+// methods: borrowBook() - set available to false 
+// returnBook() → set available to true
+
+// getStatus() → log if it's available or not
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.isAvailable = true; // default: available
+  }
+
+  borrowBook() {
+    if (this.isAvailable) {
+      this.isAvailable = false;
+      console.log(`You borrowed "${this.title}"`);
+    } else {
+      console.log(`"${this.title}" is currently not available.`);
+    }
+  }
+
+  returnBook() {
+    this.isAvailable = true;
+    console.log(`You returned "${this.title}"`);
+  }
+
+  getStatus() {
+    console.log(`"${this.title}" is ${this.isAvailable ? "Available" : "Not Available"}`);
+  }
+}
+
+// Test
+const book1 = new Book("Atomic Habits", "James Clear");
+
+book1.getStatus();     // Available
+book1.borrowBook();    // You borrowed "Atomic Habits"
+book1.getStatus();     // Not Available
+book1.returnBook();    // You returned "Atomic Habits"
+book1.getStatus();     // Available
+
+// Create a class user with username and password 
+// method: login (inputPassword) - ture or false 
+// changePasword(oldpass, newpass)  only if old matches 
+
+
+class User{
+    #password;
+    constructor (username, password){
+        this.username = username;
+        this.#password = password // private
+    }
+
+    login(inputpassword){
+        return this.#password === inputpassword;
+    }
+    changePassword(oldPass, newPass){
+        if(this.#password === oldPass){
+            this.#password = newPass;
+            console.log("password changed successfully.");
+        } else{ 
+            console.log("incorrect current password.")
+        }
+    }
+}
+
+// Test 
+const u1 = new User ("saikat", "12345");
+
+console.log(u1.login("wrong"));
+console.log(u1.login("12345"));
+u1.changePassword("wrong", "67890");
+u1.changePassword("12345", "67890");
+console.log(u1.login("67890")); 
